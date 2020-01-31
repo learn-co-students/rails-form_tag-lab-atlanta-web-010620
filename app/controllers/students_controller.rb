f5909fdd5d6f86de4eb9a5636b83e29d8505c19c
tylergreason <tyler.greason@protonmail.com>
2020-01-31 08:17:24 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new 
+
+  end 
+
+  def create 
+    @new_student = Student.create(first_name: params[:student][:first_name],last_name: params[:student][:last_name])
+    render('new')
+    # new_student.save
+    # redirect_to students_path
+  end 
 end
